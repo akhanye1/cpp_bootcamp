b461afdf6f7dfa81dbb3752abbc8f81261e4342b
Katleho <akhanye@student.fr.42>
2018-06-03 19:15:59 +0200

    changs

---
+++ day00/ex01/phonebook.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include "phonebook.hpp"
+
+void    add_contact(phonebook contacts) {
+    if (contacts::items == 0) {
+        std::cout << "Phonebook is empty" << std::endl;;
+    }
+}
+
+int     main(void) {
+    std::string command;
+    bool        exit_function;
+    phonebook   contacts[8];
+
+    command = "start";
+    contacts::items = 0;
+    exit_function = false;
+    while (!exit_function) {
+        std::cout << "phonebook>";
+        std::getline(std::cin, command);
+        if (command.compare("EXIT") == 0)
+            exit_function = true;
+        else if (command.compare("ADD") == 0)
+            add_contact(contacts)
+        std::cout << std::endl;
+    }
+}
---
+++ day00/ex01/phonebook.hpp
@@ -0,0 +1,24 @@
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include <iostream>
+
+class phonebook {
+    private:
+    std::string first_name;
+    std::string last_name;
+    std::string nickname;
+    std::string login;
+    std::string postal;
+    std::string email;
+    std::string phone;
+    std::string birthdate;
+    std::string fav_meal;
+    std::string underwear_color;
+    std::string darkest_secret;
+
+    public:
+    static int  items;
+};
+
+#endif
